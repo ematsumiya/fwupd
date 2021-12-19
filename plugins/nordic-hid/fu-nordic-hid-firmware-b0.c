e3d141917472819a14d40d2b5c1efa27032d0a78
Denis Pynkin <denis.pynkin@collabora.com>
2021-12-19 22:48:35 +0100

    plugins/nordic-hid: simple validation for mcuboot images

--- plugins/nordic-hid/fu-nordic-hid-firmware-b0.c
+++ plugins/nordic-hid/fu-nordic-hid-firmware-b0.c
@@ -27,6 +27,10 @@ fu_nordic_hid_firmware_b0_write(FuFirmware *firmware, GError **error)
 	fu_byte_array_append_uint32(buf, UPDATE_IMAGE_MAGIC_COMMON, G_LITTLE_ENDIAN);
 	fu_byte_array_append_uint32(buf, UPDATE_IMAGE_MAGIC_FWINFO, G_LITTLE_ENDIAN);
 	fu_byte_array_append_uint32(buf, UPDATE_IMAGE_MAGIC_NRF52, G_LITTLE_ENDIAN);
+	fu_byte_array_append_uint32(buf, 0x00, G_LITTLE_ENDIAN);
+	fu_byte_array_append_uint32(buf, 0x00, G_LITTLE_ENDIAN);
+	/* version */
+	fu_byte_array_append_uint32(buf, 0x63, G_LITTLE_ENDIAN);
 	blob = fu_firmware_get_bytes(firmware, error);
 	if (blob == NULL)
 		return NULL;
@@ -35,13 +39,21 @@ fu_nordic_hid_firmware_b0_write(FuFirmware *firmware, GError **error)
 }
 
 static gboolean
-fu_nordic_hid_firmware_b0_read_fwinfo(guint8 const *buf, gsize bufsz, GError **error)
+fu_nordic_hid_firmware_b0_read_fwinfo(FuFirmware *firmware,
+				      guint8 const *buf,
+				      gsize bufsz,
+				      GError **error)
 {
 	guint32 magic_common;
 	guint32 magic_fwinfo;
 	guint32 magic_compat;
 	guint32 offset;
 	guint32 hdr_offset[5] = {0x0000, 0x0200, 0x400, 0x800, 0x1000};
+	guint8 ver_major = 0;
+	guint8 ver_minor = 0;
+	guint16 ver_rev = 0;
+	guint32 ver_build_nr = 0;
+	g_autofree gchar *version = NULL;
 
 	/* find correct offset to fwinfo */
 	for (guint32 i = 0; i < G_N_ELEMENTS(hdr_offset); i++) {
@@ -67,12 +79,28 @@ fu_nordic_hid_firmware_b0_read_fwinfo(guint8 const *buf, gsize bufsz, GError **e
 						G_LITTLE_ENDIAN,
 						error))
 			return FALSE;
+		/* version */
+		if (!fu_common_read_uint32_safe(buf,
+						bufsz,
+						offset + 0x14,
+						&ver_build_nr,
+						G_LITTLE_ENDIAN,
+						error))
+			return FALSE;
+
 		if (magic_common != UPDATE_IMAGE_MAGIC_COMMON ||
 		    magic_fwinfo != UPDATE_IMAGE_MAGIC_FWINFO)
 			continue;
 		switch (magic_compat) {
 		case UPDATE_IMAGE_MAGIC_NRF52:
 		case UPDATE_IMAGE_MAGIC_NRF53:
+			/* currently only the build number is saved into the image */
+			version = g_strdup_printf("%u.%u.%u.%u",
+						  ver_major,
+						  ver_minor,
+						  ver_rev,
+						  ver_build_nr);
+			fu_firmware_set_version(firmware, version);
 			return TRUE;
 		default:
 			break;
@@ -110,7 +138,7 @@ fu_nordic_hid_firmware_b0_parse(FuFirmware *firmware,
 		return FALSE;
 	}
 
-	return fu_nordic_hid_firmware_b0_read_fwinfo(buf, bufsz, error);
+	return fu_nordic_hid_firmware_b0_read_fwinfo(firmware, buf, bufsz, error);
 }
 
 static void
--- plugins/nordic-hid/fu-nordic-hid-firmware-mcuboot.c
+++ plugins/nordic-hid/fu-nordic-hid-firmware-mcuboot.c
@@ -8,23 +8,9 @@
 
 #include "fu-nordic-hid-firmware-mcuboot.h"
 
-typedef struct __attribute__((packed)) {
-	guint8 major;
-	guint8 minor;
-	guint16 revision;
-	guint32 build_num;
-} FuNordicHidFirmwareMcubootVersion;
-
-typedef struct __attribute__((packed)) {
-	guint32 magic;
-	guint32 load_addr;
-	guint16 hdr_size;
-	guint16 protect_tlv_size;
-	guint32 img_size;
-	guint32 flags;
-	FuNordicHidFirmwareMcubootVersion ver;
-	guint32 _padding;
-} FuNordicHidFirmwareMcubootHeader;
+#define IMAGE_MAGIC		  0x96f3b83d
+#define IMAGE_TLV_INFO_MAGIC	  0x6907
+#define IMAGE_TLV_PROT_INFO_MAGIC 0x6908
 
 struct _FuNordicHidFirmwareMcuboot {
 	FuNordicHidFirmwareCommonClass parent_instance;
@@ -37,8 +23,106 @@ G_DEFINE_TYPE(FuNordicHidFirmwareMcuboot,
 static GBytes *
 fu_nordic_hid_firmware_mcuboot_write(FuFirmware *firmware, GError **error)
 {
-	/* TODO: write the test blob */
-	return NULL;
+	g_autoptr(GByteArray) buf = g_byte_array_new();
+	g_autoptr(GBytes) blob = fu_firmware_get_bytes(firmware, error);
+
+	if (blob == NULL)
+		return NULL;
+
+	/* https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/mcuboot/design.html#image-format
+	 */
+	fu_byte_array_append_uint32(buf, IMAGE_MAGIC, G_LITTLE_ENDIAN);
+	/* load_addr */
+	fu_byte_array_append_uint32(buf, 0x00, G_LITTLE_ENDIAN);
+	/* hdr_size */
+	fu_byte_array_append_uint16(buf, 0x20, G_LITTLE_ENDIAN);
+	/* protect_tlv_size */
+	fu_byte_array_append_uint16(buf, 0x00, G_LITTLE_ENDIAN);
+	/* img_size */
+	fu_byte_array_append_uint32(buf, (guint32)g_bytes_get_size(blob), G_LITTLE_ENDIAN);
+	/* flags */
+	fu_byte_array_append_uint32(buf, 0x00, G_LITTLE_ENDIAN);
+	/* version */
+	fu_byte_array_append_uint8(buf, 0x01);
+	fu_byte_array_append_uint8(buf, 0x02);
+	fu_byte_array_append_uint16(buf, 0x03, G_LITTLE_ENDIAN);
+	fu_byte_array_append_uint32(buf, 0x63, G_LITTLE_ENDIAN);
+	/* pad */
+	fu_byte_array_append_uint32(buf, 0xffffffff, G_LITTLE_ENDIAN);
+	/* payload */
+	fu_byte_array_append_bytes(buf, blob);
+	/* TLV magic and total */
+	fu_byte_array_append_uint16(buf, IMAGE_TLV_INFO_MAGIC, G_LITTLE_ENDIAN);
+	fu_byte_array_append_uint16(buf, 0x00, G_LITTLE_ENDIAN);
+
+	return g_byte_array_free_to_bytes(g_steal_pointer(&buf));
+}
+
+/* simple validation of the image */
+static gboolean
+fu_nordic_hid_firmware_mcuboot_validate(FuFirmware *firmware,
+					guint8 const *buf,
+					gsize bufsz,
+					GError **error)
+{
+	guint32 magic;
+	guint16 hdr_size;
+	guint32 img_size;
+	guint8 ver_major;
+	guint8 ver_minor;
+	guint16 ver_rev;
+	guint32 ver_build_nr;
+	guint16 magic_tlv;
+	g_autofree gchar *version = NULL;
+
+	if (!fu_common_read_uint32_safe(buf, bufsz, 0, &magic, G_LITTLE_ENDIAN, error))
+		return FALSE;
+	if (magic != IMAGE_MAGIC) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INVALID_FILE,
+				    "incorrect image magic");
+		return FALSE;
+	}
+	/* ignore load_addr */
+	if (!fu_common_read_uint16_safe(buf, bufsz, 8, &hdr_size, G_LITTLE_ENDIAN, error))
+		return FALSE;
+	/* ignore protect_tlv_size */
+	if (!fu_common_read_uint32_safe(buf, bufsz, 12, &img_size, G_LITTLE_ENDIAN, error))
+		return FALSE;
+
+	/* ignore TLVs themselves
+	 * https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/mcuboot/design.html#protected-tlvs
+	 * check the magic values only */
+	if (!fu_common_read_uint16_safe(buf,
+					bufsz,
+					hdr_size + img_size,
+					&magic_tlv,
+					G_LITTLE_ENDIAN,
+					error))
+		return FALSE;
+	if (magic_tlv != IMAGE_TLV_INFO_MAGIC && magic_tlv != IMAGE_TLV_PROT_INFO_MAGIC) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INVALID_FILE,
+				    "incorrect TLV info magic");
+		return FALSE;
+	}
+
+	/* version */
+	if (!fu_common_read_uint8_safe(buf, bufsz, 0x14, &ver_major, error))
+		return FALSE;
+	if (!fu_common_read_uint8_safe(buf, bufsz, 0x15, &ver_minor, error))
+		return FALSE;
+	if (!fu_common_read_uint16_safe(buf, bufsz, 0x16, &ver_rev, G_LITTLE_ENDIAN, error))
+		return FALSE;
+	if (!fu_common_read_uint32_safe(buf, bufsz, 0x18, &ver_build_nr, G_LITTLE_ENDIAN, error))
+		return FALSE;
+	version = g_strdup_printf("%u.%u.%u.%u", ver_major, ver_minor, ver_rev, ver_build_nr);
+
+	fu_firmware_set_version(firmware, version);
+
+	return TRUE;
 }
 
 static gboolean
@@ -65,9 +149,7 @@ fu_nordic_hid_firmware_mcuboot_parse(FuFirmware *firmware,
 		return FALSE;
 	}
 
-	/* TODO: simple validation of the image */
-
-	return TRUE;
+	return fu_nordic_hid_firmware_mcuboot_validate(firmware, buf, bufsz, error);
 }
 
 static void
